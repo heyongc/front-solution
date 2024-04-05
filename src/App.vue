6236c33e52c345332655ce80237115ae018733b5
heyongchao <528195677@qq.com>
2024-04-05 19:55:20 +0800

    Message: 构建 VueRouter、建立移动端路由表

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
-  <div>{{ isMobileTerminal }}</div>
+  <!-- 一级路由出口 -->
+  <router-view></router-view>
 </template>
 
 <script setup>
-import { isMobileTerminal } from './utils/flexible'
+// import { isMobileTerminal } from './utils/flexible'
 </script>
 
 <style lang="scss" scoped></style>
---
+++ src/views/main/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="">这是首页的内容</div>
+</template>
+
+<script setup>
+import { defineOptions } from 'vue'
+defineOptions({ name: 'index' })
+</script>
+
+<style lang="scss" scoped></style>
