558cc29e904561fac0ae3d5d9cf508082a49625a
heyongchao <528195677@qq.com>
2024-04-06 15:25:47 +0800

    Message: 动态rem基准

--- src/utils/request.js
+++ src/utils/request.js
@@ -7,11 +7,24 @@ const service = axios.create({
   timeout: 5000
 })
 
+console.log('【import.meta.env.VITE_BASE_API】', import.meta.env.VITE_BASE_API)
+
+// 请求拦截器
 service.interceptors.request.use((config) => {
   config.headers.icode = 'helloqianduanxunlianying'
   return config
 })
 
-console.log('【import.meta.env.VITE_BASE_API】', import.meta.env.VITE_BASE_API)
+// 响应拦截器
+service.interceptors.response.use((response) => {
+  const { success, message, data } = response.data
+  //   要根据success的成功与否决定下面的操作
+  if (success) {
+    return data
+  } else {
+    // TODO：业务错误
+    return Promise.reject(new Error(message))
+  }
+})
 
 export default service
