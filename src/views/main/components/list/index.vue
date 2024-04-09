b4551da1dcbe1f1740a8472ca81ac78b56523c02
heyongchao <528195677@qq.com>
2024-04-09 20:17:21 +0800

    Message: 10 构建基础列表展示

---
+++ src/views/main/components/list/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <item-vue v-for="item in pexelsList" :key="item.id" :data="item"></item-vue>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { getPexelsList } from '@/api/pexels'
+import itemVue from './item.vue'
+
+/**
+ * 构建数据请求
+ */
+let query = {
+  page: 1,
+  size: 20
+}
+const pexelsList = ref([])
+const getPexelsData = async () => {
+  const res = await getPexelsList(query)
+  pexelsList.value = res.list
+}
+getPexelsData()
+</script>
