6aaee19406c7147d81ebd61d9c62805395330a29
heyongchao <528195677@qq.com>
2024-04-09 15:51:55 +0800

    Message: 08 navigationBar 响应式实现

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,20 @@ import { createStore } from 'vuex'
 import getters from './getters'
 import category from './modules/category'
 
+import createdPersistedState from 'vuex-persistedstate'
+
 const store = createStore({
   getters,
   modules: {
     category
-  }
+  },
+  plugins: [
+    createdPersistedState({
+      // 保存到 localStorage 中的 key
+      key: 'imooc-front',
+      paths: ['category']
+    })
+  ]
 })
 
 export default store
--- src/views/layout/components/main.vue
+++ src/views/layout/components/main.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="">main</div>
+  <router-view></router-view>
 </template>
 
 <script setup>
