5e4c95223d2c8c08ecd2e2a0b17c6182666d2770
heyongchao <528195677@qq.com>
2024-04-11 16:08:42 +0800

    Message: searchBar 搜索功能完善

--- src/views/layout/components/header/header-search/index.vue
+++ src/views/layout/components/header/header-search/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="w-full">
-    <m-search v-model="inputValue">
+    <m-search
+      v-model="inputValue"
+      @search="onSearchHandler"
+      @clear="onSearchHandler"
+    >
       <template #dropdown>
         <!-- 搜索提示 -->
         <hint-vue
@@ -22,14 +26,24 @@
 <script setup>
 import { defineOptions, ref } from 'vue'
 import hintVue from './hint.vue'
+import historyVue from './history.vue'
+import { useStore } from 'vuex'
 
 defineOptions({ name: 'header-search' })
 
 const inputValue = ref('')
+const store = useStore()
 
 // 搜索的回调事件
 const onSearchHandler = (val) => {
+  console.log('【onSearchHandler】', val)
   inputValue.value = val
+  if (val) {
+    // 保存历史记录
+    store.commit('search/addHistory', val)
+    // 触发 searchText 变化
+    store.commit('app/changeSearchText', val)
+  }
 }
 </script>
 
