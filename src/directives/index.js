efab07c1bf9f630234ce1419e7149a680b5dd70d
heyongchao <528195677@qq.com>
2024-04-11 10:48:42 +0800

    Mssage: 10 vite自动指令注册完善

--- src/directives/index.js
+++ src/directives/index.js
@@ -2,10 +2,11 @@
  * 全局指令注册
  */
 export default {
-  async install(app) {
+  install(app) {
     // https://cn.vitejs.dev/guide/features.html#glob-import
     // import.meta.globEager 为同步导入
     const directives = import.meta.globEager('./modules/*.js')
+    console.log('【directives】', directives)
     for (const [key, value] of Object.entries(directives)) {
       // 拼接组件注册的 name
       const arr = key.split('/')
@@ -15,3 +16,21 @@ export default {
     }
   }
 }
+
+// /**
+//  * 全局注册指令
+//  */
+// export default {
+//   async install(app) {
+//     const directives = import.meta.glob('./modules/*.js')
+//     console.log('【directives】', directives)
+//     for (const [key, value] of Object.entries(directives)) {
+//       const d = await value()
+//       // 拼接组件注册的 name
+//       const arr = key.split('/')
+//       const directiveName = arr[arr.length - 1].replace('.js', '')
+//       // 完成注册
+//       app.directive(directiveName, d.default)
+//     }
+//   }
+// }
