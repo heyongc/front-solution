92b663b5b35edc5acfaf721588f8252fd5672692
heyongchao <528195677@qq.com>
2024-04-12 15:58:05 +0800

    Message: 13 基于GSAP实现高阶路由过渡动画

--- src/views/main/components/list/item.vue
+++ src/views/main/components/list/item.vue
@@ -5,6 +5,7 @@
       :style="{
         backgroundColor: randomRGB()
       }"
+      @click="onToPinsClick"
     >
       <!-- :src="data.photo" -->
       <img
@@ -64,8 +65,8 @@
 import { randomRGB } from '@/utils/color'
 import { saveAs } from 'file-saver'
 import { message } from '@/libs'
-import { useFullscreen } from '@vueuse/core'
-import { ref } from 'vue'
+import { useElementBounding, useFullscreen } from '@vueuse/core'
+import { ref, computed } from 'vue'
 
 const props = defineProps({
   data: {
@@ -78,7 +79,35 @@ const props = defineProps({
   }
 })
 
+const emits = defineEmits(['click'])
+
 const imgTarget = ref(null)
+
+/**
+ * pins跳转处理，记录图片的中心位置（X|Y位置 + 宽|高的一半）
+ */
+const {
+  x: imgContainerX,
+  y: imgContainerY,
+  width: imgContainerWidth,
+  height: imgContainerHeight
+} = useElementBounding(imgTarget)
+const imgContainerCenter = computed(() => {
+  return {
+    translateX: parseInt(imgContainerX.value + imgContainerWidth.value / 2),
+    translateY: parseInt(imgContainerY.value + imgContainerHeight.value / 2)
+  }
+})
+
+/**
+ * 进入详情点击事件
+ */
+const onToPinsClick = () => {
+  emits('click', {
+    id: props.data.id,
+    location: imgContainerCenter.value
+  })
+}
 
 /**
  * 下载按钮点击事件
